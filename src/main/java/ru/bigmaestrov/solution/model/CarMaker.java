a20eb3ee6cfe0b6c371ff7660a2c48105532dc28
BigMaestrov <nikita.egorov40k@gmail.com>
2021-10-21 14:32:12 +0400

    Add class Car.

--- src/main/java/ru/bigmaestrov/solution/model/CarMaker.java
+++ src/main/java/ru/bigmaestrov/solution/model/CarMaker.java
@@ -1,5 +1,16 @@
 package ru.bigmaestrov.solution.model;
 
+import lombok.Data;
+
+@Data
 public class CarMaker {
 
+    private String carModel;
+    private String carMaker;
+    private String carModelYear;
+    private String color;
+    @Override
+    public String toString(){
+        return getCarModel()+" "+getCarMaker()+" "+getCarModelYear()+" "+getColor();
+    }
 }
